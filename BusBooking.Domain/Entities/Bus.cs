4444803033584b0d3d7a458ec3add3e8b68741fa
Sanufer <Sanufer.ms@gmail.com>
2025-08-14 23:41:53 +0530

    Updated Bus Model

--- BusBooking.Domain/Entities/Bus.cs
+++ BusBooking.Domain/Entities/Bus.cs
@@ -8,12 +8,16 @@ namespace BusBooking.Domain.Entities
     public class Bus
     {
         public Guid Id { get; private set; } = Guid.NewGuid();
-        public Guid OperatorId { get; private set; }
         public string BusNumber { get; private set; } = default!;
         public Enums.BusType BusType { get; private set; }
+        public Guid OperatorId { get; private set; }
+        public BusOperator Operator { get; private set; } = default!;
         public int TotalSeats { get; private set; }
         // Seat layout is stored as JSON (rows/columns, labels, deck, etc.)
         public string SeatLayoutJson { get; private set; } = "{}";
+        private readonly List<Trip> _trips = new();
+        public IReadOnlyCollection<Trip> Trips => _trips;
+
         private Bus() { }
         public Bus(Guid operatorId, string busNumber, Enums.BusType type, int totalSeats, string layoutJson)
         {
---
+++ BusBooking.Infrastructure/Migrations/20250814174401_BusAndTripRelation.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusBooking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class BusAndTripRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Buses_BusOperators_BusOperatorId",
+                table: "Buses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Buses_BusOperatorId",
+                table: "Buses");
+
+            migrationBuilder.DropColumn(
+                name: "BusOperatorId",
+                table: "Buses");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trips_BusId",
+                table: "Trips",
+                column: "BusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Buses_OperatorId",
+                table: "Buses",
+                column: "OperatorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Buses_BusOperators_OperatorId",
+                table: "Buses",
+                column: "OperatorId",
+                principalTable: "BusOperators",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Trips_Buses_BusId",
+                table: "Trips",
+                column: "BusId",
+                principalTable: "Buses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Buses_BusOperators_OperatorId",
+                table: "Buses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Trips_Buses_BusId",
+                table: "Trips");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Trips_BusId",
+                table: "Trips");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Buses_OperatorId",
+                table: "Buses");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "BusOperatorId",
+                table: "Buses",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Buses_BusOperatorId",
+                table: "Buses",
+                column: "BusOperatorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Buses_BusOperators_BusOperatorId",
+                table: "Buses",
+                column: "BusOperatorId",
+                principalTable: "BusOperators",
+                principalColumn: "Id");
+        }
+    }
+}
