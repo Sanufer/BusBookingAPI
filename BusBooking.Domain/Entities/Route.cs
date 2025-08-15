fa068a6a9fcf2f6380b9ef0b1e524c843281930d
Sanufer <Sanufer.ms@gmail.com>
2025-08-15 21:42:25 +0530

    Updated Route Table

--- BusBooking.Domain/Entities/Route.cs
+++ BusBooking.Domain/Entities/Route.cs
@@ -4,7 +4,9 @@ namespace BusBooking.Domain.Entities
     {
         public Guid Id { get; private set; } = Guid.NewGuid();
         public Guid SourceCityId { get; private set; }
+        public City SourceCity { get; private set; } = default!;
         public Guid DestinationCityId { get; private set; }
+        public City DestinationCity { get; private set; } = default!;
         public double DistanceKm { get; private set; }
         private Route() { }
         public Route(Guid source, Guid destination, double km) { SourceCityId = source; DestinationCityId = destination; DistanceKm = km; }
---
+++ BusBooking.Infrastructure/Migrations/20250815161146_UpdateRouteModel.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusBooking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateRouteModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Routes_DestinationCityId",
+                table: "Routes",
+                column: "DestinationCityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Routes_SourceCityId",
+                table: "Routes",
+                column: "SourceCityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Routes_Cities_DestinationCityId",
+                table: "Routes",
+                column: "DestinationCityId",
+                principalTable: "Cities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Routes_Cities_SourceCityId",
+                table: "Routes",
+                column: "SourceCityId",
+                principalTable: "Cities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Routes_Cities_DestinationCityId",
+                table: "Routes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Routes_Cities_SourceCityId",
+                table: "Routes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Routes_DestinationCityId",
+                table: "Routes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Routes_SourceCityId",
+                table: "Routes");
+        }
+    }
+}
